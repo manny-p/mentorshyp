21be920105f38b736bd8082ddd35cde4da54257f
Manny <hi@mannyparra.com>
2020-09-20 14:40:03 -0600

    edit mdx blog post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,7 +8,7 @@ import {Layout} from '../../src/components/blog/Layout'
 export default function BlogPage() {
   return (
       <div sx={{variant: 'layout.container'}}>
-        <Layout pageTitle="Blog" description="Mentorshyp Blog">
+        <Layout pageTitle="Blog" description="Mentorshyp Blog"/>
         <header sx={{variant: 'layout.nav'}}>
           <Nav/>
         </header>
@@ -26,7 +26,6 @@ export default function BlogPage() {
             })}
           </main>
         </section>
-        </Layout>
       </div>
   )
 }
