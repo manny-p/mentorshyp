f349c4b7e78d0bffbb488070bcb76a12f18db09f
Manny <hi@mannyparra.com>
2020-09-20 23:57:46 -0600

    add inactive video component

--- src/components/user/dashboard/main/VideoActive.js
+++ src/components/user/dashboard/main/VideoActive.js
@@ -3,22 +3,26 @@ import {jsx} from 'theme-ui'
 import Iframe from 'react-iframe'
 import {useContext} from 'react'
 import {Context} from '../../../../../store'
+import Inactive from './event/Inactive'
 
 export default function VideoActive() {
 
   const {call} = useContext(Context)
 
   return (
-      <Iframe
-          url="/room/calling"
-          display={call ? 'block' : 'none'}
-          sx={{
-            height: '50vh',
-            width: '80vw',
-            position: 'absolute',
-            // zIndex: '10',
-          }}
-      >
-      </Iframe>
+      <div>
+        <Inactive/>
+        <Iframe
+            url="/room/calling"
+            display={call ? 'block' : 'none'}
+            sx={{
+              height: '50vh',
+              width: '80vw',
+              position: 'absolute',
+              // zIndex: '10',
+            }}
+        >
+        </Iframe>
+      </div>
   )
 }
